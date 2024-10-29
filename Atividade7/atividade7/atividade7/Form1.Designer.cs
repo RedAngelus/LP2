5f8d312509dea6c4373ffc1ac0fd9b57e1d8ec56
Luiz Felipe Faustino Soares <162517830+RedAngelus@users.noreply.github.com>
2024-10-29 11:13:25 -0300

    Add files via upload

---
+++ Atividade7/atividade7/atividade7/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace atividade7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bexercicio1 = new System.Windows.Forms.Button();
+            this.bexercicio2 = new System.Windows.Forms.Button();
+            this.bexercicio3 = new System.Windows.Forms.Button();
+            this.bexercicio4 = new System.Windows.Forms.Button();
+            this.bexercicio5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bexercicio1
+            // 
+            this.bexercicio1.Location = new System.Drawing.Point(166, 44);
+            this.bexercicio1.Name = "bexercicio1";
+            this.bexercicio1.Size = new System.Drawing.Size(163, 119);
+            this.bexercicio1.TabIndex = 0;
+            this.bexercicio1.Text = "Exercicio 1";
+            this.bexercicio1.UseVisualStyleBackColor = true;
+            this.bexercicio1.Click += new System.EventHandler(this.bexercicio1_Click);
+            // 
+            // bexercicio2
+            // 
+            this.bexercicio2.Location = new System.Drawing.Point(401, 44);
+            this.bexercicio2.Name = "bexercicio2";
+            this.bexercicio2.Size = new System.Drawing.Size(163, 119);
+            this.bexercicio2.TabIndex = 1;
+            this.bexercicio2.Text = "Exercicio 2";
+            this.bexercicio2.UseVisualStyleBackColor = true;
+            this.bexercicio2.Click += new System.EventHandler(this.bexercicio2_Click);
+            // 
+            // bexercicio3
+            // 
+            this.bexercicio3.Location = new System.Drawing.Point(166, 174);
+            this.bexercicio3.Name = "bexercicio3";
+            this.bexercicio3.Size = new System.Drawing.Size(163, 119);
+            this.bexercicio3.TabIndex = 2;
+            this.bexercicio3.Text = "Exercicio 3";
+            this.bexercicio3.UseVisualStyleBackColor = true;
+            this.bexercicio3.Click += new System.EventHandler(this.bexercicio3_Click);
+            // 
+            // bexercicio4
+            // 
+            this.bexercicio4.Location = new System.Drawing.Point(401, 174);
+            this.bexercicio4.Name = "bexercicio4";
+            this.bexercicio4.Size = new System.Drawing.Size(163, 119);
+            this.bexercicio4.TabIndex = 3;
+            this.bexercicio4.Text = "Exercicio 4";
+            this.bexercicio4.UseVisualStyleBackColor = true;
+            // 
+            // bexercicio5
+            // 
+            this.bexercicio5.Location = new System.Drawing.Point(277, 299);
+            this.bexercicio5.Name = "bexercicio5";
+            this.bexercicio5.Size = new System.Drawing.Size(163, 119);
+            this.bexercicio5.TabIndex = 4;
+            this.bexercicio5.Text = "Exercicio 5";
+            this.bexercicio5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bexercicio5);
+            this.Controls.Add(this.bexercicio4);
+            this.Controls.Add(this.bexercicio3);
+            this.Controls.Add(this.bexercicio2);
+            this.Controls.Add(this.bexercicio1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bexercicio1;
+        private System.Windows.Forms.Button bexercicio2;
+        private System.Windows.Forms.Button bexercicio3;
+        private System.Windows.Forms.Button bexercicio4;
+        private System.Windows.Forms.Button bexercicio5;
+    }
+}
+
---
+++ Atividade7/atividade7/atividade7/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace atividade7
+{
+    public partial class Form1 : Form
+    {
+        string auxiliar = " ";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bexercicio1_Click(object sender, EventArgs e)
+        {
+            int[] Vetor = new int[20];
+
+            auxiliar = "";
+
+            for (int i = 0; i < 20;  i++)
+            {
+                auxiliar = Interaction.InputBox("Digite o numero.", "Entrada de Dados.");
+
+                if (!int.TryParse(auxiliar, out Vetor[i]))
+                {
+                    MessageBox.Show("numero invalido!");
+                    i--;
+                }
+            }
+
+            Array.Reverse(Vetor);
+            auxiliar = " ";
+            foreach (int x in Vetor)
+            {
+                auxiliar += x + "\n";
+            }
+
+            MessageBox.Show(auxiliar);
+        }
+
+        private void bexercicio2_Click(object sender, EventArgs e)
+        {
+            ArrayList Lista = new ArrayList()
+            {
+                "Ana", "André", "Débora", "Fátima", "João", "Janete", "Otávio", "Marcelo", "Pedro", "Thais"
+            };
+            Lista.Remove("Otávio");
+            foreach (string x in Lista)
+            {
+                auxiliar += x + "\n";
+            }
+            MessageBox.Show(auxiliar);
+        }
+
+        private void bexercicio3_Click(object sender, EventArgs e)
+        {
+            double[,] notas = new double[20, 3];
+            double[] media = new double[20];
+            auxiliar = "";
+
+            for ( int i = 0; i < 20; i++)
+            {
+                for ( int j = 0; j < 3; j++)
+                {
+                    auxiliar = Interaction.InputBox("Digite a nota " + (j+1), "Nota do aluno " + (i+1));
+
+                    if (!double.TryParse(auxiliar, out notas[i, j] ))
+                    {
+                        MessageBox.Show("Valor invalido");
+                        j--;
+                    }
+                    else if (notas[i, j] < 0 || notas[i, j] > 10)
+                    {
+                        MessageBox.Show("Inserir um valor entre 0 a 10");
+                        j--;
+                    }
+                    else
+                    media[i] = notas[i, j] + notas[i, j] + notas[i, j];
+                }
+                media[i] = media[i] / 3;
+
+                foreach ( double y in media ) 
+
+                auxiliar = auxiliar + "nota do aluno " + (i + 1) + ": " + media[] + "\n";
+            }
+            MessageBox.Show(auxiliar);
+        }
+    }
+}
